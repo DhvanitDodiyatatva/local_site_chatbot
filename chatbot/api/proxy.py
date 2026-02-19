12223866a45019f43e58011f188bedf21f604b63
Dhvanit_Dodiya <dhvanit.dodiya@tatvasoft.com>
2026-02-19 14:39:16 +0530

    backend url update

--- chatbot/api/proxy.py
+++ chatbot/api/proxy.py
@@ -1,7 +1,7 @@
 import frappe
 import requests
 
-AI_BACKEND_URL = "http://localhost:8001/erp-chat"
+AI_BACKEND_URL = "http://192.168.2.29:8001/erp-chat"
 
 @frappe.whitelist()
 def forward_to_ai(question):
