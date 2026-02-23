f9332990d96efc5e9b8ecf9758d80df76a0811e2
Dhvanit_Dodiya <dhvanit.dodiya@tatvasoft.com>
2026-02-23 09:43:53 +0530

    tables for consumer, products and ConsumerProduct and ingest real life data in maria_db

---
+++ chatbot/chatbot/doctype/consumerproduct/test_consumerproduct.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2026, dhvanit and Contributors
+# See license.txt
+
+# import frappe
+from frappe.tests.utils import FrappeTestCase
+
+
+class TestConsumerProduct(FrappeTestCase):
+	pass
